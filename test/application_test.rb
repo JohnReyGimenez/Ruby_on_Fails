3a76ee87e13665152f3dac725b0f9195183fc417
JohnReyGimenez <gimenezjr5@gmail.com>
2025-11-19 22:42:38 +0800

    added tests

---
+++ test/application_test.rb
@@ -0,0 +1,16 @@
+require_relative 'test_helper'
+
+class TestApp < RubyOnFails::Application
+end
+class RubyOnFailsAppTest < Minitest::Test
+  include Rack::Test::Methods
+  def app
+    TestApp.new
+  end
+  def test_request
+    get "/"
+    assert last_response.ok?
+    body = last_response.body
+    assert body["Hello"]
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,5 @@
+$LOAD_PATH.unshift File.expand_path("../../lib",
+ __dir__)
+require "ruby_on_fails"
+require "rack/test"
+require "minitest/autorun"
