22a4fddafeb37707ee71187c8453c7046fc8d9f9
JohnReyGimenez <gimenezjr5@gmail.com>
2025-11-19 21:10:17 +0800

    Initialize Application class and update gem config

--- lib/ruby_on_fails.rb
+++ lib/ruby_on_fails.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
-require_relative "ruby_on_fails/version"
+require_relative 'ruby_on_fails/version'
 
 module RubyOnFails
+  # The main application class used to handle Rack requests
+  class Application
+    def call(_env)
+      [200, { 'Content-Type' => 'text/html' },
+       ['Hello from Ruby on Rulers!']]
+    end
+  end
+
   class Error < StandardError; end
   # Your code goes here...
 end
